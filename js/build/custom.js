8fe50c541c9a34bc641e0fbadc8d76ba85c2c216
Poojan-98 <chapagain.pujan@gmail.com>
2022-07-24 09:38:36 +0545

    style: coderefinement

--- js/build/custom.js
+++ js/build/custom.js
@@ -2,60 +2,26 @@ jQuery(document).ready(function ($) {
     /* Desktop Navigation
     --------------------------------------------- */
     $('.menu-item-has-children').append('<button class="submenu-toggle-btn"><i class="fas fa-caret-down"></i></button>');
-    $('.site-header .secondary-nav >div').prepend('<button class="close-btn"></button>');
+
     $('.submenu-toggle-btn').on('click', function () {
         $(this).siblings('.sub-menu').stop().slideToggle();
         $(this).toggleClass('active');
     });
 
-    $('.header-main .toggle-btn').on('click', function () {
-        $(this).siblings('.main-navigation').animate({
-            width: 'toggle'
-        });
-    });
-    $('.main-navigation .close-btn').on('click', function () {
-        $('.main-navigation').animate({
-            width: 'toggle'
-        });
-    });
 
     /* Mobile Navigation
     --------------------------------------------- */
+    $('.site-header  .header-bottom-slide-inner ').css("top", 0);
 
-    var adminbarHeight = $('#wpadminbar').outerHeight();
-    if (adminbarHeight) {
-        $('.site-header .mobile-header .header-bottom-slide .header-bottom-slide-inner ').css("top", adminbarHeight);
-    } else {
-        $('.site-header .mobile-header .header-bottom-slide .header-bottom-slide-inner ').css("top", 0);
-    }
-
-    $('.sticky-header .toggle-btn,.site-header .mobile-header .toggle-btn-wrap .toggle-btn').click(function () {
+    $('.site-header .mobile-header .toggle-btn-wrap .toggle-btn').click(function () {
         $('body').addClass('mobile-menu-active');
-        $('.site-header .mobile-header .header-bottom-slide .header-bottom-slide-inner ').css("transform", "translate(0,0)");
+        $('.site-header .mobile-header .header-bottom-slide-inner ').css("transform", "translate(0,0)");
     });
-    $('.site-header .mobile-header .header-bottom-slide .header-bottom-slide-inner .container .mobile-header-wrapper  .close-btn').click(function () {
+    $('.site-header .mobile-header .header-bottom-slide-inner .mobile-header-wrapper  .close-btn').click(function () {
         $('body').removeClass('mobile-menu-active');
-        $('.site-header .mobile-header .header-bottom-slide .header-bottom-slide-inner ').css("transform", "translate(-100%,0)");
+        $('.site-header .mobile-header .header-bottom-slide-inner ').css("transform", "translate(-100%,0)");
     });
 
-
-
-    /* secondary Navigation
-    --------------------------------------------- */
-
-    $('.site-header .secondary-nav .toggle-btn, .secondary-nav .close-btn').on('click', function () {
-        if ($('#wpadminbar').length) {
-            $('.site-header .secondary-nav > div').animate({
-                width: 'toggle',
-                'top': adminbarHeight
-            });
-        } else {
-            $('.site-header .secondary-nav > div').animate({
-                width: 'toggle'
-            });
-        }
-
-    });
 
     // AOS animation
     AOS.init({
