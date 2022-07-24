62653e1f1d388f0fd699bb622feb5bac0c000238
Poojan-98 <chapagain.pujan@gmail.com>
2022-07-24 12:09:07 +0545

    style: scoll slider

--- js/build/custom.js
+++ js/build/custom.js
@@ -28,6 +28,34 @@ jQuery(document).ready(function ($) {
         // once: true,
     });
 
+
+    // scroll slider
+
+    const slider = document.querySelector('.beneficiaries .beneficiaries-grid');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
 });
 
 
